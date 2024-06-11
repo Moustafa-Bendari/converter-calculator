4a9c9ac9fc92a1a356dc682802bff20a4c076faf
Andrew Eardley <eardley.andrew1@gmail.com>
2024-06-11 10:46:12 -0600

    merge

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 Vue.createApp({
     data() {
         return {
-
+            mbAnswer: "",
+            bytesInput: ""
 
         }
     },
     methods: {
+        submitBytes() {
+            this.$ref.
+        }
 
 
     },
